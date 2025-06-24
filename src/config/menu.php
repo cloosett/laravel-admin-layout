c49efdbc8e2828667fea16470d3a67604bf42b6d
cloosett <samper.goss@gmail.com>
2025-06-24 13:20:57 +0300

    1

--- src/config/menu.php
+++ src/config/menu.php
@@ -7,14 +7,12 @@ return [
             'route' => 'dashboard.index',
             'icon' => 'bi-speedometer2',
             'active_pattern' => 'dashboard*',
-            'permission' => null,
         ],
         [
             'title' => 'Users',
             'route' => 'users.index',
             'icon' => 'bi-people',
             'active_pattern' => 'users*',
-            'permission' => 'manage_users',
         ],
     ],
 ]; 
